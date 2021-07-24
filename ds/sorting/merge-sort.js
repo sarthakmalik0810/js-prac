855deaed2f70b9ec1d3e0fc91d53892cb07ed0ab
Sarthak Malik <sarthakmalik0810@gmail.com>
2021-07-25 01:15:24 +0530

    merge-sort

--- ds/sorting/merge-sort.js
+++ ds/sorting/merge-sort.js
@@ -21,3 +21,11 @@ const merge = (arr1, arr2) => {
   }
   return results;
 };
+
+const mergeSort = arr => {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+};
