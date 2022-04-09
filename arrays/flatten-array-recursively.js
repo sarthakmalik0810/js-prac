905a16846d063c7dcf2bdf93eeb8068cfa906550
Sarthak Malik <sarthak.malik@simpplr.com>
2022-04-09 07:29:30 +0530

    added flattenArray with level

--- arrays/flatten-array-recursively.js
+++ arrays/flatten-array-recursively.js
@@ -4,7 +4,7 @@
 
 function flatten(arr) {
   let acc = [];
-  arr.forEach(ele => {
+  arr.forEach((ele) => {
     if (Array.isArray(ele)) {
       acc = acc.concat(flatten(ele));
     } else {
@@ -16,8 +16,21 @@ function flatten(arr) {
 
 const arr = [
   [1, 2],
-  [3, 4, [5, 6], []],
+  [3, 4, [5, 6, [7, [8]]]],
 ];
 
-const flattened = flatten(arr);
+function flattenWithLevel(arr, level = 1) {
+  let acc = [];
+  arr.forEach((ele) => {
+    if (Array.isArray(ele) && level > 0) {
+      acc = [...acc, ...flattenWithLevel(ele, level - 1)];
+    } else {
+      acc.push(ele);
+    }
+  });
+
+  return acc;
+}
+
+const flattened = flattenWithLevel(arr);
 console.log(flattened);
