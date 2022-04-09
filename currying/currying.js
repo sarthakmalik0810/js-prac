82c8cbf323ea54a672995929dcf1447389c6ea91
Sarthak Malik <sarthak.malik@simpplr.com>
2022-04-09 07:29:12 +0530

    added curryWrapper

--- currying/currying.js
+++ currying/currying.js
@@ -1,114 +1,124 @@
-
 function math(num) {
-  
-  this.value = function() {
+  this.value = function () {
     return num;
-  }
+  };
 
-  this.add = function(addNum) {
-    let res = num + addNum
+  this.add = function (addNum) {
+    let res = num + addNum;
     return math(res);
-  }
+  };
 
-  this.sub = function(subNum) {
+  this.sub = function (subNum) {
     let res = num - subNum;
     return math(res);
-  }
+  };
 
   return this;
 }
 
-let a = math(6).add(4).sub(2).value()
-
+let a = math(6).add(4).sub(2).value();
 
 // using toString
 
-function getSum (...args) {
-  let total = 0
-  function sum (...args) {
+function getSum(...args) {
+  let total = 0;
+  function sum(...args) {
     for (const item of args) {
-      total += item
+      total += item;
     }
-    return sum
+    return sum;
   }
 
   sum.toString = function () {
-    return total
-  }
+    return total;
+  };
 
-  return sum(...args)
+  return sum(...args);
 }
 
-console.log(0 + getSum(1, 2)(4, 3)(4)(5)())
+console.log(0 + getSum(1, 2)(4, 3)(4)(5)());
 
 // using valueOf
 
-function getSumV2 (...args) {
-  let total = 0
-  function sum (...args) {
+function getSumV2(...args) {
+  let total = 0;
+  function sum(...args) {
     for (const item of args) {
-      total += item
+      total += item;
     }
-    return sum
+    return sum;
   }
 
   sum.valueOf = function () {
-    return total
-  }
+    return total;
+  };
 
-  return sum(...args)
+  return sum(...args);
 }
 
-console.log(0 + getSumV2(1, 2)(4, 3)(4)(5))
+console.log(0 + getSumV2(1, 2)(4, 3)(4)(5));
 
 // using extra ()
 
-function getSumV3 (...args) {
-  let total = 0
-  function sum (...args) {
+function getSumV3(...args) {
+  let total = 0;
+  function sum(...args) {
     for (const item of args) {
-      total += item
+      total += item;
     }
     if (args.length === 0) {
-      return total
+      return total;
     }
-    return sum
+    return sum;
   }
 
-  return sum(...args)
+  return sum(...args);
 }
 
-console.log(getSumV3(1, 2)(4, 3)(4)(5)())
-
-
+console.log(getSumV3(1, 2)(4, 3)(4)(5)());
 
 // let sum = a => b => b ? sum(a+b) : a;
 
 // let problem = sum(1)(2)(3)(14)();
 // console.log(problem);
 
-
 // addition(6,1,2,3,4)(1)(23, 5,6, 7)....(1).end(); // create addition method
 
-function getSumV4 (...args) {
-  let total = 0
-  function sum (...args) {
+function getSumV4(...args) {
+  let total = 0;
+  function sum(...args) {
     for (const item of args) {
-      total += item
+      total += item;
     }
-    return sum
+    return sum;
   }
 
-  sum.end = function() {
-      return total;
-  }
- 
+  sum.end = function () {
+    return total;
+  };
+
   sum.valueOf = function () {
-    return total
-  }
+    return total;
+  };
 
-  return  sum(...args)
+  return sum(...args);
 }
 
 console.log(0 + getSumV4(1, 2)(4, 3)(4)(5).end());
 
+function curryWrapper(fn) {
+  function curr(...args) {
+    if (args.length >= fn.length) {
+      return fn(...args);
+    }
+    return (...args2) => curr(...args, ...args2);
+  }
+
+  return curr;
+}
+
+function multiply(x, y, z) {
+  return x * y * z;
+}
+
+const curried = curryWrapper(multiply);
