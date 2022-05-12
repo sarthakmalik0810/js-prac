620606e7511cdaa5ceb3cdc339c1bb30deef9405
Sarthak Malik <72226593+sarthakmalik0810@users.noreply.github.com>
2022-05-12 10:13:23 +0530

    class inheritance

--- OOPS/script.js
+++ OOPS/script.js
@@ -11,8 +11,36 @@ function Student(id, name, age) {
   Person.call(this, name, age);
   this.id = id;
 }
+
+Student.prototype.getId = function() {
+  console.log(this.id);
+}
+
 Student.prototype = Object.create(Person.prototype);
+
 Student.prototype.constructor = Student;
+
+class Person {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+  }
+
+  getAge() {
+    console.log(this.age)
+  }
+}
+
+class Student extends Person {
+  constructor(name, age, id) {
+    super(name, age);
+    this.id = id;
+  }
+
+  getId() {
+    console.log(this.id);
+  }
+}
 // Object.defineProperty(Student.prototype, 'constructor', {
 //   value: Student,
 //   enumerable: false,
